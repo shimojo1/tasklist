d18b5ac8b98d65d3aa4dcaa20e3bc3005b9b5a4d
shimojo <shimojo@no1s.biz>
2024-04-08 16:14:36 +0900

    21.5.5まで

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -3,24 +3,15 @@ package jp.gihyo.projava.tasklist;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value="/hello")
-	@ResponseBody
-	String hello() {
-		return """
-				<html>
-				    <head><title>Hello</title></head>
-				    <body>
-				        <h1>hello</h1>
-				        It works!<br>
-				        現在時刻は%sです。
-				    </body>
-				</html>
-				""".formatted(LocalDateTime.now());
+	String hello(Model model) {
+		model.addAttribute("time", LocalDateTime.now());
+		return "hello";
 	}
 
 }

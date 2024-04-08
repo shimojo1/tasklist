d5f09b97e2821dbea888f62a889f021b8e833949
shimojo <shimojo@no1s.biz>
2024-04-08 16:25:49 +0900

    21.5.7まで

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,17 +1,29 @@
 package jp.gihyo.projava.tasklist;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	record TaskItem(String id, String task, String deadline, boolean done) {}
+	private List<TaskItem> taskItems = new ArrayList<>();
+	
 	@RequestMapping(value="/hello")
 	String hello(Model model) {
 		model.addAttribute("time", LocalDateTime.now());
 		return "hello";
+	}
+	
+	@GetMapping("/list")
+	String listItems(Model model) {
+		model.addAttribute("taskList", taskItems);
+		return "home";
 	}
 
 }

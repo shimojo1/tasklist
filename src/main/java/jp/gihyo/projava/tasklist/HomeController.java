8a7aef8980675a6674d89cf07df8980c3383d29a
shimojo <shimojo@no1s.biz>
2024-04-08 16:28:53 +0900

    21.5.8まで

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -3,11 +3,13 @@ package jp.gihyo.projava.tasklist;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -24,6 +26,15 @@ public class HomeController {
 	String listItems(Model model) {
 		model.addAttribute("taskList", taskItems);
 		return "home";
+	}
+	
+	@GetMapping("/add")
+	String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
+		String id = UUID.randomUUID().toString().substring(0, 8);
+		TaskItem item = new TaskItem(id, task, deadline, false);
+		taskItems.add(item);
+		
+		return "redirect:/list";
 	}
 
 }

62d42e957a021821fccdfe1b79127c7c535bf8d8
shimojo <shimojo@no1s.biz>
2024-04-10 17:31:50 +0900

    22.3.5まで

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 	record TaskItem(String id, String task, String deadline, boolean done) {}
 	private List<TaskItem> taskItems = new ArrayList<>();
+	private final TaskListDao dao;
+	
+	@Autowired
+	HomeController(TaskListDao dao) {
+		this.dao = dao;
+	}
 	
 	@RequestMapping(value="/hello")
 	String hello(Model model) {
@@ -24,6 +31,7 @@ public class HomeController {
 	
 	@GetMapping("/list")
 	String listItems(Model model) {
+		List<TaskItem> taskItems = dao.findAll();
 		model.addAttribute("taskList", taskItems);
 		return "home";
 	}
@@ -32,7 +40,7 @@ public class HomeController {
 	String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
 		String id = UUID.randomUUID().toString().substring(0, 8);
 		TaskItem item = new TaskItem(id, task, deadline, false);
-		taskItems.add(item);
+		dao.add(item);
 		
 		return "redirect:/list";
 	}
